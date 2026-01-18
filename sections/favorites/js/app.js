6d46d816e58817b5637f9c518adb1a1323b25ee2
abdulaziz <telegrampreimum4@gmail.com>
2026-01-18 11:28:10 +0500

    #upgraded_ui

--- sections/favorites/js/app.js
+++ sections/favorites/js/app.js
@@ -1,34 +1,33 @@
-let apiLimit=15;
-let apiSkip=0;
-
-function skeletonUI (bool=true,limit=10) {
-    if(bool==true) {
-        document.querySelector(".js-more-down").style.display="none";
-        Array.from({length:limit})
-        .forEach(()=>{
-            let clone = document.getElementById("car_card_skeleton_template").cloneNode(true).content;
-            document.querySelector(".js-cards-skeleton-box").append(clone);
-        })
+favoritesListChecker();
+function favoritesListChecker() {
+    if(localStorage.getItem("favorites_list")) {
+        let arr = JSON.parse(localStorage.getItem("favorites_list"));
+        if(arr.length!=0) {
+            skeletonUI(true,arr.length);
+            let datasArr=[];
+            arr.forEach(id=>{
+            fetch(`https://json-api.uz/api/project/fn44-amaliyot/cars/${id}`)
+            .then(res=>res.json())
+            .then(res=>{
+                datasArr.push(res);
+                if(datasArr.length==arr.length) {
+                    cardWrite(datasArr);
+                }
+            });
+            })
+        } else {
+            document.getElementById("favorites_empty_info").classList.remove("hidden");
+            document.getElementById("favorites_empty_info").classList.add("flex");
+        }
     } else {
-        document.querySelector(".js-cards-skeleton-box").innerHTML=null;
-        document.querySelector(".js-more-down").style.display="flex";
+        document.getElementById("favorites_empty_info").classList.remove("hidden");
+        document.getElementById("favorites_empty_info").classList.add("flex");
     }
 };
 
-mainRequest();
-function mainRequest() {
-skeletonUI(true,apiLimit);
-fetch(`https://json-api.uz/api/project/fn44-amaliyot/cars?limit=${apiLimit}&skip=${apiSkip}`)
-.then(res=>res.json())
-.then(res=>{
-    skeletonUI(false,0);
-    cardWrite(res.data,res.total)
-})
-
-function cardWrite(response,total) {
-    let elArray = [];
-    response.forEach(el=>elArray.push(el));
-    elArray.forEach(el=>{
+function cardWrite(data) {
+    skeletonUI(false);
+    data.forEach(el=>{
         let clone=document.getElementById("car_card_template").cloneNode(true).content;
         clone.querySelector(`[data-txt="name"]`).textContent=el.name?el.name:"No data";
         clone.querySelector(`[data-txt="year"]`).textContent=el.year?el.year:"No data";
@@ -52,11 +51,6 @@ function cardWrite(response,total) {
         }
         document.querySelector(".js-cards-main-box").append(clone);
     })
-    elArray=[];
-    if(apiSkip>total) {
-        document.querySelector(".js-more-down").style.display="none";
-    }
-    document.querySelector(".js-more-down").disabled=false;
     document.querySelectorAll(".js-car-more-detail-btn")
     .forEach(el=>el.addEventListener("click",(evt)=>{
         detailsUiWrite(evt.target.getAttribute("data-detail-id"));
@@ -67,7 +61,6 @@ function cardWrite(response,total) {
         document.querySelector(".js-more-loader").classList.add("opacity-[1]");
         },1200)
     }));
-    
     document.querySelectorAll(".js-car-add-favorite-btn")
     .forEach(el=>{
         el.addEventListener("click",
@@ -90,14 +83,21 @@ function cardWrite(response,total) {
             }
         })
     })
-};
+}
+
+function skeletonUI (bool=true,limit=1) {
+    if(bool==true) {
+        Array.from({length:limit})
+        .forEach(()=>{
+            let clone = document.getElementById("car_card_skeleton_template").cloneNode(true).content;
+            document.querySelector(".js-cards-skeleton-box").append(clone);
+        })
+    } else {
+        document.querySelector(".js-cards-skeleton-box").innerHTML=null;
+    }
 };
 
-document.querySelector(".js-more-down").addEventListener("click",()=>{
-    apiSkip+=apiLimit;
-    document.querySelector(".js-more-down").disabled=true;
-    mainRequest();
-});
+
 function detailsUiWrite(id) {
 
 fetch(`https://json-api.uz/api/project/fn44-amaliyot/cars/${id}`)
@@ -164,12 +164,10 @@ function isLogined() {
 AuthChecker()
 function AuthChecker() {
 if(isLogined()==true) {
-    document.querySelector(".js-login-btn").hidden=true;
     document.querySelector(".js-auth-managment ").hidden=false;
     document.getElementById("main_box_container").classList.remove("mb-[0px]","sm:mb-[0px]");
     document.getElementById("main_box_container").classList.add("mb-[50px]","sm:mb-[65px]");
 } else {
-    document.querySelector(".js-login-btn").hidden=false;
     document.querySelector(".js-auth-managment ").hidden=true;
     document.getElementById("main_box_container").classList.remove("mb-[50px]","sm:mb-[65px]");
     document.getElementById("main_box_container").classList.add("mb-[0px]","sm:mb-[0px]");
@@ -177,32 +175,6 @@ if(isLogined()==true) {
 }
 
 /* Favorites Logic */
-
-function mainpulationFavorites(id) {
-    if(localStorage.getItem("favorites_list")) {
-        let old_list=JSON.parse(localStorage.getItem("favorites_list"));
-        let old_uniqe_list = new Set(old_list);
-        let new_list = Array.from(old_uniqe_list);
-        new_list.push(Number(id));
-        let new_uniqe_list = new Set(new_list);
-        localStorage.setItem("favorites_list",JSON.stringify(Array.from(new_uniqe_list)));
-        successAlert("Istaklar ro'yxatiga qo'shildi!");
-        document.querySelector(`[data-favorite-id='${id}']`).setAttribute("data-tip","Istaklardan o'chirish");
-        document.querySelector(`[data-favorite-id='${id}']`).innerHTML=
-        `<svg class="pointer-events-none" width="22" height="22" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.35248 4.90532C1.35248 2.94498 2.936 1.35248 4.89346 1.35248C6.25769 1.35248 6.86058 1.92336 7.50002 2.93545C8.13946 1.92336 8.74235 1.35248 10.1066 1.35248C12.064 1.35248 13.6476 2.94498 13.6476 4.90532C13.6476 6.74041 12.6013 8.50508 11.4008 9.96927C10.2636 11.3562 8.92194 12.5508 8.00601 13.3664C7.94645 13.4194 7.88869 13.4709 7.83291 13.5206C7.64324 13.6899 7.3568 13.6899 7.16713 13.5206C7.11135 13.4709 7.05359 13.4194 6.99403 13.3664C6.0781 12.5508 4.73641 11.3562 3.59926 9.96927C2.39872 8.50508 1.35248 6.74041 1.35248 4.90532Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>`;
-        document.querySelector(`[data-favorite-id='${id}']`).classList.remove("favorited_false");
-        document.querySelector(`[data-favorite-id='${id}']`).classList.add("favorited_true");
-    } else {
-        let x = [];x.push(Number(id));
-        localStorage.setItem("favorites_list",JSON.stringify(x));
-        successAlert("Istaklar ro'yxatiga qo'shildi!");
-        document.querySelector(`[data-favorite-id='${id}']`).setAttribute("data-tip","Istaklardan o'chirish");
-        document.querySelector(`[data-favorite-id='${id}']`).innerHTML=
-        `<svg class="pointer-events-none" width="22" height="22" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.35248 4.90532C1.35248 2.94498 2.936 1.35248 4.89346 1.35248C6.25769 1.35248 6.86058 1.92336 7.50002 2.93545C8.13946 1.92336 8.74235 1.35248 10.1066 1.35248C12.064 1.35248 13.6476 2.94498 13.6476 4.90532C13.6476 6.74041 12.6013 8.50508 11.4008 9.96927C10.2636 11.3562 8.92194 12.5508 8.00601 13.3664C7.94645 13.4194 7.88869 13.4709 7.83291 13.5206C7.64324 13.6899 7.3568 13.6899 7.16713 13.5206C7.11135 13.4709 7.05359 13.4194 6.99403 13.3664C6.0781 12.5508 4.73641 11.3562 3.59926 9.96927C2.39872 8.50508 1.35248 6.74041 1.35248 4.90532Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>`;
-        document.querySelector(`[data-favorite-id='${id}']`).classList.remove("favorited_false");
-        document.querySelector(`[data-favorite-id='${id}']`).classList.add("favorited_true");
-    }
-};
 
 function removeElementFavorites(id) {
     let old_data = JSON.parse(localStorage.getItem("favorites_list"));
@@ -212,11 +184,11 @@ function removeElementFavorites(id) {
     })
     localStorage.setItem("favorites_list",JSON.stringify(new_filtered_array));
     successAlert("Istaklar ro'yxatidan o'chirildi!");
-    document.querySelector(`[data-favorite-id='${id}']`).setAttribute("data-tip","Istaklarga qo'shish");
-    document.querySelector(`[data-favorite-id='${id}']`).innerHTML=
-    `<svg class="pointer-events-none" width="22" height="22" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4.89346 2.35248C3.49195 2.35248 2.35248 3.49359 2.35248 4.90532C2.35248 6.38164 3.20954 7.9168 4.37255 9.33522C5.39396 10.581 6.59464 11.6702 7.50002 12.4778C8.4054 11.6702 9.60608 10.581 10.6275 9.33522C11.7905 7.9168 12.6476 6.38164 12.6476 4.90532C12.6476 3.49359 11.5081 2.35248 10.1066 2.35248C9.27059 2.35248 8.81894 2.64323 8.5397 2.95843C8.27877 3.25295 8.14623 3.58566 8.02501 3.88993C8.00391 3.9429 7.98315 3.99501 7.96211 4.04591C7.88482 4.23294 7.7024 4.35494 7.50002 4.35494C7.29765 4.35494 7.11523 4.23295 7.03793 4.04592C7.01689 3.99501 6.99612 3.94289 6.97502 3.8899C6.8538 3.58564 6.72126 3.25294 6.46034 2.95843C6.18109 2.64323 5.72945 2.35248 4.89346 2.35248ZM1.35248 4.90532C1.35248 2.94498 2.936 1.35248 4.89346 1.35248C6.0084 1.35248 6.73504 1.76049 7.20884 2.2953C7.32062 2.42147 7.41686 2.55382 7.50002 2.68545C7.58318 2.55382 7.67941 2.42147 7.79119 2.2953C8.265 1.76049 8.99164 1.35248 10.1066 1.35248C12.064 1.35248 13.6476 2.94498 13.6476 4.90532C13.6476 6.74041 12.6013 8.50508 11.4008 9.96927C10.2636 11.3562 8.92194 12.5508 8.00601 13.3664C7.94645 13.4194 7.88869 13.4709 7.83291 13.5206C7.64324 13.6899 7.3568 13.6899 7.16713 13.5206C7.11135 13.4709 7.05359 13.4194 6.99403 13.3664C6.0781 12.5508 4.73641 11.3562 3.59926 9.96927C2.39872 8.50508 1.35248 6.74041 1.35248 4.90532Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>`;
-    document.querySelector(`[data-favorite-id='${id}']`).classList.remove("favorited_true");
-    document.querySelector(`[data-favorite-id='${id}']`).classList.add("favorited_false");
+    document.querySelector(`[data-favorite-id='${id}']`).closest(".js-car-card").remove();
+    if(document.querySelectorAll(".js-car-card").length==0) {
+            document.getElementById("favorites_empty_info").classList.remove("hidden");
+            document.getElementById("favorites_empty_info").classList.add("flex");
+    }
 }
 
 /* UI Features */
